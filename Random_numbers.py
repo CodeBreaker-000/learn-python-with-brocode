0910783359cf7f100e1307298003ed7394d755e3
CodeBreaker-000 <mohammedabdulnaeem0.1.0@gmail.com>
2025-06-29 20:51:12 +0600

    Date: 29/06/25

---
+++ Random_numbers.py
@@ -0,0 +1,25 @@
+# Random Numbers
+
+import random
+
+# print(help(random))
+
+# low = 1
+# high = 100
+# number = random.randint(low, high)
+
+# print(number)
+
+# number = random.random()    # it will give a random floating number between 1 and 100
+# print(number)
+
+# options = ("rock", "paper", "scissors")
+# option = random.choice(options)
+# print(option)
+
+cards =  ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+random.shuffle(cards)
+print(cards)
+
+
+
---
+++ dictionary.py
@@ -0,0 +1,53 @@
+# dictionary = a collection of {key:value} pairs. Ordered and changable.
+#              No duplicates.    
+
+capitals = {
+    "USA": "Washington D.C.",
+    "India": "New Delhi",
+    "China": "Beijing",
+    "Russia": "Moscow"
+    }
+
+# print(dir(capitals))
+# print(help(capitals))
+
+# print(capitals.get("China"))
+# print(capitals.get("Japan"))
+
+# if capitals.get("Russia"):
+#     print("That capital exists")
+# else:
+#     print("That capital doesn't exists")
+
+# capitals.update({"Germany": "Berlin"})      # Adds new key-value pair
+# capitals.update({"USA": "Detroit"}) 
+# print(capitals)
+
+# capitals.pop("China")
+# print(capitals)
+
+# capitals.popitem()   # pops the last key-value pair in a dictionary
+# print(capitals)
+
+# capitals.clear()
+# print(capitals)
+
+# keys = capitals.keys()
+# # print(keys)
+# for key in keys:
+#     print(key)
+
+# # values = capitals.values()
+# # print(values)
+# for value in capitals.values():
+#     print(value)
+
+# items = capitals.items()
+# # print(items)
+# for key, value in capitals.items():
+#     print(f"{key}: {value}")
+
+
+
+
+

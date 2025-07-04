fca2ee517df2201e158486f62cfff963a3513cb9
CodeBreaker-000 <mohammedabdulnaeem0.1.0@gmail.com>
2025-07-04 19:27:49 +0600

    Date: 04/07/25

---
+++ iterables.py
@@ -0,0 +1,44 @@
+# Iterables = An object/collection that can return its elements one at a time,
+#             allowing ut to be iterated over in a loop
+
+# numbers = [1, 2, 3, 4, 5]
+
+# # for number in numbers:
+# #     print(number)
+
+# # for item in numbers:
+# #     print(item)
+
+# for item in reversed(numbers):
+#     # print(item, end=" ")
+#     print(item, end="-")
+
+# numbers = (1, 2, 3, 4, 5)
+
+# for number in numbers:
+#     print(number)
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit)
+
+# name = "Bro Code"
+
+# for character in name:
+#     print(character, end=" ")
+
+
+my_dictionary = {"A": 1, "B": 2, "C": 3}
+
+# for key in my_dictionary.keys():
+#     print(key)
+
+# for value in my_dictionary.values():
+#     print(value)
+
+for key, value in my_dictionary.items():
+    print(f"{key} : {value}")
+
+
+
